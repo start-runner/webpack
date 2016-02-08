3a198525aed0aebcd59afa191985a90cea62d94f
deepsweet <kir@soulshine.in>
2016-02-08 13:34:38 +0700

    :space_invader: add basic tests

---
+++ test/lib/index.js
@@ -0,0 +1,47 @@
+import test from 'tape';
+
+import * as task from '../../lib/';
+
+test('build: basic', t => {
+    t.equal(
+        typeof task.build,
+        'function',
+        '1st function'
+    );
+
+    t.equal(
+        typeof task.build(),
+        'function',
+        '2nd function'
+    );
+
+    t.equal(
+        typeof task.build()(),
+        'function',
+        '3rd function'
+    );
+
+    t.end();
+});
+
+test('dev: basic', t => {
+    t.equal(
+        typeof task.dev,
+        'function',
+        '1st function'
+    );
+
+    t.equal(
+        typeof task.dev(),
+        'function',
+        '2nd function'
+    );
+
+    t.equal(
+        typeof task.dev()(),
+        'function',
+        '3rd function'
+    );
+
+    t.end();
+});
